f90664ce3f0b64a8abd83e7017a704bf44ec0e61
Omar <omarlucky0@gmail.com>
2026-02-04 16:47:04 +0100

    chore: hide footer in the planner page

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,14 +1,17 @@
 import Navbar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
+import { useLocation } from 'react-router-dom';
 
 export default function MainLayout({ children }) {
+  //get current path
+  const location = useLocation();
+  const isPlannerPage = location.pathname === '/planner';
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-
       <main className="flex-1">{children}</main>
-
-      <Footer />
+      {!isPlannerPage && <Footer />}
     </div>
   );
 }
